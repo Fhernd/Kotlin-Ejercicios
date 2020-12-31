0488f3b352079e70d3550e47835cedbda87dd19d
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-12-31 09:30:20 -0500

    Kotlin Ejercicio: 29 Sobrescribir el Método de Instancia toString() de una Clase Personalizada
    Sobrescribir el Método de Instancia toString() de una Clase Personalizada con el lenguaje de programación Kotlin.

--- Kotlin-Ejercicios-Parte01/src/main/kotlin/ejercicio029/sobrescritura_metodo_instancia.kt
+++ Kotlin-Ejercicios-Parte01/src/main/kotlin/ejercicio029/sobrescritura_metodo_instancia.kt
@@ -1,2 +1,19 @@
 package ejercicio029
 
+// Ejercicio 29: Sobrescribir el método de instancia toString() de una clase personalizada.
+
+class Computador (var id: Int, var marca: String, var cpu: String, var ram: Int, var ssd: Int) {
+    override fun toString() = "ID: $id - Marca: $marca - CPU: $cpu - RAM: $ram - SSD: $ssd"
+}
+
+fun main() {
+    var computadorGamer = Computador(1001, "MSi", "Intel Core i7", 32, 720)
+
+    // Antes:
+    // ejercicio029.Computador@28d93b30
+
+    println(computadorGamer)
+
+    // Después:
+    // ID: 1001 - Marca: MSi - CPU: Intel Core i7 - RAM: 32 - SSD: 720
+}
