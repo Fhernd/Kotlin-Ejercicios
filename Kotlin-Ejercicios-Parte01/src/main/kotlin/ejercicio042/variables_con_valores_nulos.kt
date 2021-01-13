564077a5a7bada51eba86f5c2069c7a7086f739f
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-13 17:49:37 -0500

    Kotlin Ejercicio: 42 Permitir la Asignación de Valores Nulos (null) Sobre una Variable
    Permitir la Asignación de Valores Nulos (null) Sobre una Variable con el lenguaje de programación Kotlin.

---
+++ Kotlin-Ejercicios-Parte01/src/main/kotlin/ejercicio042/variables_con_valores_nulos.kt
@@ -0,0 +1,17 @@
+package ejercicio042
+
+// Ejercicio 42: Permitir la asignación de valores nulos (null) sobre una variable.
+
+fun main() {
+    var nombre : String?
+
+    nombre = "Oliva"
+
+    println(nombre)
+
+    println()
+
+    nombre = null
+
+    println(nombre)
+}
