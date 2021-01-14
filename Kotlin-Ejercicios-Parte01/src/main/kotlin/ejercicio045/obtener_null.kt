95a4240ff0fab332e34e03febab31776dbc156a3
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-14 14:27:58 -0500

    Kotlin Ejercicio: 45 Evitar Errores al Acceder a Atributos de una Propiedad que Sea null
    Evitar Errores al Acceder a Atributos de una Propiedad que Sea null con el lenguaje de programación Kotlin.

--- Kotlin-Ejercicios-Parte01/src/main/kotlin/ejercicio045/obtener_null.kt
+++ Kotlin-Ejercicios-Parte01/src/main/kotlin/ejercicio045/obtener_null.kt
@@ -1,5 +1,21 @@
 package ejercicio045
 
-// Ejercicio 45: Evitar errores al acceder a atributos de una propiedad que sea null.
+// Ejercicio 45: Evitar errores al acceder a atributos de una propiedad que sea null.   
 
+class Persona(val primerNombre: String, val segundoNombre: String?, val primerApellido: String)
 
+fun main() {
+    val julio = Persona("Julio", null, "Pérez")
+
+    val alexander = Persona("Alexander", "Iván", "Robledo")
+
+    var cantidadCaracteres = julio.segundoNombre?.length
+
+    println(cantidadCaracteres) // null
+
+    println()
+
+    cantidadCaracteres = alexander.segundoNombre?.length
+
+    println(cantidadCaracteres)
+}
